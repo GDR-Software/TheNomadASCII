a621d658faf41a78326f6cc98f8e3870015cabdf
SIGAAMDAD <86020846+SIGAAMDAD@users.noreply.github.com>
2022-08-17 08:04:51 -0500

    Update playr.h

--- src/playr.h
+++ src/playr.h
@@ -7,7 +7,8 @@ This file is part of The Nomad (ASCII) source code
 
 The Nomad (ASCII) source code is distrubuted as free software. You can di-
 stribute it and/or modify it, just make sure you credit me. This software
-uses the terms and policies of the SACE Source Code License.
+uses the terms and policies of the GNU General Public License as published
+by the Free Software Foundation
 
 The Nomad (ASCII) source code is distributed in hopes that it could be us-
 eful and/or helpful in your own projects. This game is meant for playing
