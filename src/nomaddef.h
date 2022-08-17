f06d3cc3ae943e45575730e7f7b3bf86bba02e71
SIGAAMDAD <86020846+SIGAAMDAD@users.noreply.github.com>
2022-08-17 07:45:54 -0500

    Update nomaddef.h

--- src/nomaddef.h
+++ src/nomaddef.h
@@ -1,10 +1,33 @@
+// Language: C++20
+/*
+##########################################################################
+Copyright(c) 2021-2022 SIGAAMDAD
+
+This file is part of The Nomad (ASCII) source code
+
+The Nomad (ASCII) source code is distrubuted as free software. You can di-
+stribute it and/or modify it, just make sure you credit me. This software
+uses the terms and policies of the SACE Source Code License.
+
+The Nomad (ASCII) source code is distributed in hopes that it could be us-
+eful and/or helpful in your own projects. This game is meant for playing
+and modification. So, have fun customizing the source code to your own pe-
+rsonal wishes!
+
+DESCRIPTION:
+	Header File Containing The Non-Class Non-Function Macros, Constants, A-
+	nd Variables
+##########################################################################
+*/
+// nomaddef.h
+
 #ifndef _NOMADDEF_H_
 #define _NOMADDEF_H_
 
 // standard ranges, doesn't apply to everything
 #define RNG_CLOSE			10
 #define RNG_MID				30
-#define RNG_LONG			45
+#define RNG_LONG				45
 #define RNG_ULTRA			60
 
 #define WMAP_NORTHWEST		1
@@ -19,7 +42,7 @@
 
 // multipliers for the game, stuff like mob spawns, hp, and also dividers for ammo spawns and buffs
 // the regular, easiest, and most kid-friendly difficulty
-#define DIF_NOOB			0
+#define DIF_NOOB				0
 // some teen content, more mature, harder
 #define DIF_RECRUIT			1
 // call of duty kind of content, as difficult as doom eternal
@@ -34,8 +57,8 @@
 // wandering nomadic tribe
 #define NOMAD_TRIBE_W		'*'
 
-#define MOUNTAIN			'∆'
-#define HILL				'^'
+#define MOUNTAIN				'∆'
+#define HILL					'^'
 #define MINERAL_DEPO		'π'
 #define TEMPLE				'Ω'
 
