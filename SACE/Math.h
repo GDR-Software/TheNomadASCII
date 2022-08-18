6cc45f3f96d94c92e489c66b1219125fcd80fa0a
SIGAAMU <111403004+SIGAAMU@users.noreply.github.com>
2022-08-18 10:27:46 -0500

    Create Math.h

---
+++ SACE/Math.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+#include "SACE.h"
+
+namespace SACE {
+	class Math
+	{
+	public:
+		float distanceBetweenOBJ(float x1, float y1, float x2, float y2);
+	};
+};
