354dfd08ed1174208553c2595be188ab6c4fec24
SIGAAMDAD <86020846+SIGAAMDAD@users.noreply.github.com>
2022-08-26 08:44:20 -0500

    Create keycodes.h

---
+++ src/keycodes.h
@@ -0,0 +1,51 @@
+#ifndef _KEYCODES_H_
+#define _KEYCODES_H_
+
+#include "nomaddef.h"
+
+#define KEY_ESC			27
+#define KEY_TAB			9
+#define KEY_ALT			132
+#define KEY_CTRL			133
+#define KEY_SHIFT		134
+#define KEY_F1			135
+#define KEY_F2			136
+#define KEY_F3			137
+#define KEY_F4			138
+#define KEY_F5			139
+#define KEY_F6			140
+#define KEY_F7			141
+#define KEY_F8			142
+#define KEY_F9			143
+#define KEY_F10			144
+#define KEY_F11			145
+#define KEY_F12			146
+
+#define KEY_A			97
+#deifne KEY_B			98
+#define KEY_C			99
+#define KEY_D			100
+#define KEY_E 			101
+#define KEY_F			102
+#define KEY_G			103
+#define KEY_H			104
+#define KEY_I			105
+#define KEY_J			106
+#define KEY_K			107
+#define KEY_L			108
+#define KEY_M			109
+#define KEY_N			110
+#define KEY_O			111
+#define KEY_P			112
+#define KEY_Q			113
+#define KEY_R			114
+#define KEY_S			115
+#define KEY_T			116
+#define KEY_U			117
+#define KEY_V			118
+#define KEY_W			119
+#define KEY_X			120
+#define KEY_Y			121
+#define KEY_Z			122
+
+#endif
