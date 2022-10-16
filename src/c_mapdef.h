d7d71412f3ce81f9ca1d833976ef302dede3031f
SIGAAMDAD <86020846+SIGAAMDAD@users.noreply.github.com>
2022-10-16 16:16:24 -0500

    Delete src directory

--- src/c_mapdef.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MAPDEF_H_
-#define _MAPDEF_H_
-
-#define M_GALAKAS_MAIN
-
-#endif
--- src/c_printScreen.cpp
+++
@@ -1,9 +0,0 @@
-using namespace Campaign;
-
-void printScreen()
-{
-	for (const auto &i : HUD)
-		std::cout << i;
-	playr = std::make_unique<Playr>();
-	playr->cmd_prompt();
-}
